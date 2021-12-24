eaf1e6e56707e8961ba8292a8521efd254eaef10
yinengmr <771453065@qq.com>
2021-12-24 11:40:02 +0800

    JS运行机制

--- docs/.vuepress/conf/sidebar.js
+++ docs/.vuepress/conf/sidebar.js
@@ -1,23 +1,15 @@
 // 左侧菜单
 module.exports = {
-    '/accumulate/': [
-        '',
-        'JS正则',
-        '前端算法和数据结构',
-        '面试总结',
-        'ES6的Generator',
-    ],
-    '/git/': [
-        '',
-    ],
-    '/linux/': [
-        '',
-        'centos7时间设置与同步',
-    ],
-    '/windows/': [
-        '',
-    ],
-    '/nginx/': [
-        '',
-    ]
-}
+  "/accumulate/": [
+    "",
+    "JS正则",
+    "前端算法和数据结构",
+    "面试总结",
+    "ES6的Generator",
+    "JS运行机制",
+  ],
+  "/git/": [""],
+  "/linux/": ["", "centos7时间设置与同步"],
+  "/windows/": [""],
+  "/nginx/": [""],
+};
